891c899fc44c461cff0b5a1ca577737637ffa0cd
MorschaVG <morschavongrumbkow@gmail.com>
2026-02-08 14:10:04 +0100

    Test unmounting cycle & update effects. Prevent memory leaks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
+import ErrorMessage from './components/errorMessage.jsx'
+import {useEffect, useState} from "react";
 
 function App() {
-  return <h1>Welkom!</h1>
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+
+    }, [text]); // A value in the dependency array makes this an 'update effect'!
+
+    return (
+        <>
+            <h1>De grote testpagina!</h1>
+            <input type="text"
+                   name="test"
+                   onChange={(e) => setText(e.target.value)}
+                   placeholder="Gaat uw gang"
+                   value={text}/>
+            {text.length > 3 && <ErrorMessage/>}
+
+        </>
+    )
 }
 
 export default App
